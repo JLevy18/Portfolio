a74502f9bfc5ef02addbcc7886d1f0be039d6ef5
JLevy18 <justinlevy20@gmail.com>
2024-07-01 21:40:43 -0400

    setup typescript

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>LevTheDev | Home</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        
+      </main>
+
+    </div>
+  )
+}
